9027f287c86388c5e88856f19461593300baa2e9
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2023-09-24 11:35:08 -0400

    construction d'une card a partir du post

--- tp1_troqueur/tp1_PierreOlivierCadeau/app.py
+++ tp1_troqueur/tp1_PierreOlivierCadeau/app.py
@@ -3,6 +3,8 @@ TP1 web 3
 """
 import re
 import bd
+import os
+import datetime
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -37,15 +39,33 @@ def index():
     )
 
 
-@app.route('/ajout_article')
+@app.route('/ajout_article', methods=["GET", "POST"])
 def ajout_article():
     """Page d'index"""
-    return render_template(
-        'formulaire.jinja',
-        titre_h1='Bonjour!',
-        message='Bienvenu sur la démo des templates!',
-        routes=lister_routes()
-    )
+
+    if request.method == "POST":
+        titre = request.form['titre']
+        description = request.form['description']
+        categorie = request.form['categorie']
+
+        # attribution de la date comme nom pour classer les images
+        maintenant = datetime.datetime.now()
+        nom_image = maintenant.strftime("%Y-%m-%d %H:%M:%S")
+
+        return render_template(
+            'article.jinja',
+            titre_h2='Les 5 derniers cas répertorié',
+            titre_h3= titre,
+            sous_titre= categorie,
+            src_image_article='../static/images/scp1471.jpg',
+            message= description,
+            routes=lister_routes()
+        )
+    else:
+        return render_template(
+            'formulaire.jinja',
+            routes=lister_routes()
+        )
 
 @app.route('/details_objet')
 def liste_article():
@@ -58,7 +78,7 @@ def liste_article():
     )
 
 
-def insertion_objet(u_id, u_titre, u_description, u_photo, u_categorie, u_commentaires):
+def insertion_objet(u_titre, u_description, u_photo, u_categorie, u_commentaires):
     """Pour démontrer une insertion"""
 
     with bd.creer_connexion() as connexion:
@@ -68,7 +88,6 @@ def insertion_objet(u_id, u_titre, u_description, u_photo, u_categorie, u_commen
                 "INSERT INTO `objets` " +
                 "(`id`, `titre`, `description`, `photo`, `categorie`) " +
                 "VALUES (" +
-                "%(u_id)s, %(u_titre)s, %(u_description)s, %(u_photo)s, %(u_categorie)s, %(u_commentaires)s )"
+                "NULL, %(u_titre)s, %(u_description)s, %(u_photo)s, %(u_categorie)s, %(u_commentaires)s )"
             )
 
-            print(f"Identifiant de l'élément ajouté : {curseur.lastrowid}")
