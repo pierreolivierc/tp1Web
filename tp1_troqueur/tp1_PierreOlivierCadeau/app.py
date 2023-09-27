6450f928b82d4f164390f66260dd1136a857c329
pierreolivierc <115761312+pierreolivierc@users.noreply.github.com>
2023-09-27 10:18:53 -0400

    page erreur

--- tp1_troqueur/tp1_PierreOlivierCadeau/app.py
+++ tp1_troqueur/tp1_PierreOlivierCadeau/app.py
@@ -27,6 +27,7 @@ app.config['CHEMIN_VERS_AJOUTS'] = os.path.join(
     *app.config['MORCEAUX_VERS_AJOUTS']
 )
 
+
 def lister_routes():
     """Liste les routes pour le menu"""
     return [
@@ -45,6 +46,47 @@ def lister_routes():
     ]
 
 
+@app.errorhandler(400)
+def page_non_trouvee(e):
+    """Page d'erreur lorsque paramètre manquant dans l'url"""
+
+    return render_template(
+        'insertion_reussi_ou_echec.jinja',
+        titre_onglet="Paramètre manquant",
+        titre_h2="Erreur",
+        alerte="alert-danger",
+        message="Paramètre manquant dans l'url." + e.description,
+        routes=lister_routes()
+    ), 400
+
+
+@app.errorhandler(404)
+def page_non_trouvee(e):
+    """Page d'erreur lorsque la page n'existe pas"""
+
+    return render_template(
+        'insertion_reussi_ou_echec.jinja',
+        titre_onglet="Page inexistante",
+        titre_h2="Erreur",
+        alerte="alert-danger",
+        message="La page que vous recherchez n'existe pas." + e.description,
+        routes=lister_routes()
+    ), 404
+
+@app.errorhandler(500)
+def erreur_serveur(e):
+    """Page d'erreur en cas d'erreur avec la base de données"""
+
+    return render_template(
+        'insertion_reussi_ou_echec.jinja',
+        titre_onglet="Erreur serveur",
+        titre_h2="Erreur",
+        alerte="alert-danger",
+        message="La connexion au serveur a échoué." + e.description,
+        routes=lister_routes()
+    ), 500
+
+
 
 @app.route('/')
 def index():
@@ -53,12 +95,13 @@ def index():
 
     return render_template(
         'accueil.jinja',
-        titre_onglet = 'Accueil',
+        titre_onglet='Accueil',
         titre_h2='Les 5 derniers produits à échéanger',
-        objets_recuperer = objets,
-        src= '../static/images/objets/',
+        objets_recuperer=objets,
+        src='../static/images/objets/',
         routes=lister_routes()
     )
+
 
 def lire_cookie():
     format = request.cookies.get("langue", default="fr_CA")
@@ -72,8 +115,6 @@ def ecriture(langue):
     return reponse
 
 
-
-
 @app.route('/details', methods=["GET", "POST"])
 def details_objet():
     if request.method == "POST":
@@ -85,7 +126,7 @@ def details_objet():
             objets_recuperer=objet,
             titre_onglet='Détails',
             date_creation=date_convertie,
-            src= '../static/images/objets/',
+            src='../static/images/objets/',
             modifier='/modifier',
             routes=lister_routes()
         )
@@ -128,7 +169,7 @@ def modifier_objet():
             ), 400
 
         nom_image = enregistrement_image()
-        if not nom_image :
+        if not nom_image:
             return render_template(
                 'insertion_reussi_ou_echec.jinja',
                 titre_onglet="Ajout d'un produit",
@@ -140,16 +181,15 @@ def modifier_objet():
 
         supprimer_image(objet["photo"])
 
-        #Modifcation de l'objet dans la base de donnée
+        # Modifcation de l'objet dans la base de donnée
         modfication_objet(titre, description, nom_image, categorie, date, id)
 
-
         return render_template(
-               'insertion_reussi_ou_echec.jinja',
+            'insertion_reussi_ou_echec.jinja',
             titre_onglet="Ajout d'une produit",
             titre_h2="Ajout d'une produit",
             alerte="alert-success",
-            message= "Le produit a bien été ajouté!",
+            message="Le produit a bien été ajouté!",
             routes=lister_routes()
         )
     else:
@@ -165,6 +205,7 @@ def modifier_objet():
             routes=lister_routes()
         )
 
+
 @app.route('/ajout_article', methods=["GET", "POST"])
 def ajout_article():
     """Page d'index"""
@@ -173,11 +214,11 @@ def ajout_article():
         titre = request.form['titre'].strip()
         description = request.form['description'].strip()
 
-        #Supprimez toutes les balises HTML
+        # Supprimez toutes les balises HTML
         titre = re.sub(r'<.*?>', '', titre)
         description = re.sub(r'<.*?>', '', description)
 
-        #récupération de l'ID catégorie
+        # récupération de l'ID catégorie
         categorie_value = request.form['categorie']
         categorie = recuperation_id_categorie(categorie_value)
 
@@ -191,14 +232,13 @@ def ajout_article():
                 routes=lister_routes()
             ), 400
 
-
         nom_image = enregistrement_image()
-        if not nom_image :
+        if not nom_image:
             return render_template(
                 'insertion_reussi_ou_echec.jinja',
                 titre_onglet="Ajout d'un produit",
                 titre_h2="Ajout d'un produit",
-                alerte= "alert-danger",
+                alerte="alert-danger",
                 message="Erreur: l'image est manquante",
                 routes=lister_routes()
             ), 400
@@ -206,17 +246,15 @@ def ajout_article():
         maintenant = datetime.now()
         date_creation = maintenant.strftime("%Y-%m-%d")
 
-
         # insertion a la bd
         insertion_objet(titre, description, nom_image, categorie, date_creation)
-
 
         return render_template(
             'insertion_reussi_ou_echec.jinja',
             titre_onglet="Ajout d'une produit",
             titre_h2="Ajout d'une produit",
             alerte="alert-success",
-            message= "Le produit a bien été ajouté!",
+            message="Le produit a bien été ajouté!",
             routes=lister_routes()
         )
     else:
@@ -227,22 +265,23 @@ def ajout_article():
             routes=lister_routes()
         )
 
+
 @app.route('/liste_tous_les_objets')
 def liste_article():
     """Faire une boucle pour afficher tous les objets"""
 
     objets = recuperation_objet()
 
-    #afficher plusieurs card
+    # afficher plusieurs card
 
     return render_template(
         'afficher_tous_les_articles.jinja',
         titre_onglet="Tous les produits",
         titre_h2='Les 5 derniers produits à échéanger',
-        objets_recuperer = objets,
-        src= '../static/images/objets/',
+        objets_recuperer=objets,
+        src='../static/images/objets/',
         routes=lister_routes()
-        )
+    )
 
 
 def insertion_objet(u_titre, u_description, u_photo, u_categorie, u_date):
@@ -294,6 +333,7 @@ def enregistrement_image():
 
     return nom_image
 
+
 def supprimer_image(objets):
     # Mettra des / ou \ dépendamment de l'OS
     chemin_complet = os.path.join(
@@ -311,7 +351,7 @@ def recuperation_id_categorie(u_categorie):
             with connexion.get_curseur() as curseur:
                 # Insertion de objet
                 curseur.execute(
-                    'SELECT id FROM `categories` '+
+                    'SELECT id FROM `categories` ' +
                     'WHERE description = %s', (u_categorie,)
                 )
 
@@ -329,8 +369,8 @@ def recuperation_objet():
                 # Sélection de tous les objets
                 curseur.execute(
                     'SELECT objets.*, categories.description AS categorie_description ' +
-                    'FROM `objets` '+
-                    'JOIN `categories` ON objets.categorie = categories.id '+
+                    'FROM `objets` ' +
+                    'JOIN `categories` ON objets.categorie = categories.id ' +
                     'ORDER BY objets.photo DESC '
                 )
                 return curseur.fetchall()
@@ -345,10 +385,10 @@ def recuperation_objet_avec_id(u_id):
             with connexion.get_curseur() as curseur:
                 # Sélection de tous les objets
                 curseur.execute(
-                    'SELECT objets.*, categories.description AS categorie_description '+
-                    'FROM `objets` '+
-                    'JOIN `categories` ON objets.categorie = categories.id '+
-                    'WHERE objets.id =  %s', (u_id, )
+                    'SELECT objets.*, categories.description AS categorie_description ' +
+                    'FROM `objets` ' +
+                    'JOIN `categories` ON objets.categorie = categories.id ' +
+                    'WHERE objets.id =  %s', (u_id,)
                 )
                 return curseur.fetchone()
     except Exception as e:
